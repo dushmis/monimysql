3ec6cc24bcfd2875fe111d0e62b1ad1e621e6dbd
dushyant <dushmis@gmail.com>
2014-07-21 12:10:11 +0530

    error handling+connection

--- monimy.py
+++ monimy.py
@@ -5,12 +5,7 @@ from moni import moni
 from connection import connection
 
 def connect_db():
-<<<<<<< HEAD
   return connection(myhost="myhost", myuser="myuser", mypasswd="mypasswd", mydb="mydb")
-=======
-  connector = MySQLdb.connect(host="IP", user="USER", passwd="PASSWORD", db="DB")
-  return {"db":connector,"cur":connector.cursor()}
->>>>>>> 04caad04b0abd3b8a279d25358b4ab035c26817d
   
 def get_list_pid():
   connector=connect_db()
